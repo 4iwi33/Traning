b99153c1ec3a6866dd67711bbdb09f6591543d50
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-22 21:14:11 +0300

    displacment
    moved all files in Traning

---
+++ Second.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Second</title>
+</head>
+
+<body>
+    <?php
+    //Написать скрипт который из пяти переменных
+    //($a, $b, $c, $d, $e) находит наибольшую.
+
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 6;
+
+    echo max($a, $b, $c, $d, $e);
+
+
+
+
+
+
+
+
+
+
+
+    ?>
+</body>
+
+</html>
