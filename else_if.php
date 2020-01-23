5176ffc80deb69ec19ea82e4a2f768771a8b5093
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-23 21:49:01 +0300

    Create else_if.php
    create else if

---
+++ else_if.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>else_if</title>
+</head>
+
+<body>
+    <?php 
+    //хорошй пример else if
+    if (4 > 4) {   
+        echo "4 больше, чем 4";
+    } elseif (4 == 4) {
+        echo "4 равен 4";
+    } else {
+        echo "4 меньше, чем 4";
+    }
+    ?>
+</body>
+
+</html>
