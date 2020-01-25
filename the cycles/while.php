2d59b4e43f59bbe1cb98711db8cc9296cbced671
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-25 16:54:51 +0300

    adding
    adding cycle

--- the cycles/while.php
+++ the cycles/while.php
@@ -12,7 +12,9 @@
     <?php
     //the cycle will work when the cycle of the cycle
     //is equally true 
-
+    while (2 + 2 == 4) {
+        echo "эта надпись выводится";
+    }
 
     ?>
 </body>
