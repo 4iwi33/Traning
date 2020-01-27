596e6dccbb13c1ddd43f2a93e55950ae878702ad
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-27 20:03:52 +0300

    creat new file
    this script dw

--- work with files/excample2.php
+++ work with files/excample2.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>exc2</title>
-</head>
-
-<body>
     <?php
-    $file = fopen("exc2.txt",  "a+t");
+    $file = fopen("exc2.txt",  "r+t");
+    while (feof($file)) {
+        echo fread($file, 1);
+    }
+    fclose($file);
 
-
-
+    // it doesn't work
     ?>
-</body>
-
-</html>
