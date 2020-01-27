dd1b76f408a2ac3ad79f26e9d57357757a761808
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-27 19:33:40 +0300

    adding
    rename + creat

--- work with files/excample1.php
+++ work with files/excample1.php
@@ -15,7 +15,7 @@
     2*3=6
     5*5=25
     6*5=30*/
-    $file = fopen("a.txt",  "a+t");
+    $file = fopen("exc1.txt",  "a+t");
     fwrite($file, "Exc\nText\nNext");
     fclose($file);
     ?>
---
+++ work with files/excample2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exc2</title>
+</head>
+
+<body>
+    <?php
+    $file = fopen("exc2.txt",  "a+t");
+
+
+
+    ?>
+</body>
+
+</html>
