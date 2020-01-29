7c78e3a493a9292a821d98c8565619eb4dc29fb6
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-01-29 19:01:20 +0300

    ss
    ss

---
+++ work with files/example3.php
@@ -0,0 +1,3 @@
+<?php
+file_put_contents("exc3.txt", "Test");
+echo file_get_contents("exc3.txt"). "<br>";
--- work with files/excample2.php
+++ work with files/excample2.php
@@ -3,7 +3,12 @@
     while (!feof($file)) {
         echo fread($file, 1);
     }
+    fseek($file, 0);
+
+    echo fread($file, 1);
+
     fclose($file);
+    echo "<hr>";
 
     // it doesn't work
     ?>
