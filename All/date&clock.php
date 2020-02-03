64565732378139af9b18290dff5e2538f041d057
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-03 20:19:43 +0300

    rename

--- All/date&clock.php
+++ All/date&clock.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>date</title>
+    <title>date&lock</title>
 </head>
 
 <body>
     <?
+    $dat = date("d.m.y");
+    echo "date $dat" .    "<br>";
+
+    $dat = date("h.i.s");
+    echo "clock $dat";
 
     ?>
 </body>
