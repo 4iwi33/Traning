b6e467376b00ba0e7d7e76fb5f945dc34feae3a4
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-05 21:59:33 +0300

    create new

---
+++ work with files/include/include.php
@@ -0,0 +1,17 @@
+<html>
+
+<body>
+    <!-- Предположим, у нас есть стандартный файл нижнего
+ колонтитула с именем "footer.php",  -->
+    <h1>Welcome to my home page!</h1>
+    <p>Some text.</p>
+    <p>Some more text.</p>
+    <?php include 'footer.php'; ?>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing
+        elit. Aspernatur, obcaecati.</p>
+    <!-- Чтобы включить файл нижнего колонтитула
+     на страницу, используйте оператор include: -->
+
+</body>
+
+</html>
