56e675e93b33621f85d522a6c552f935b90718ad
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-05 21:23:00 +0300

    add

---
+++ logical operation/array/array3.php
@@ -0,0 +1,9 @@
+<?
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) {
+    echo "Key=" ."HopHayLalaley" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+//Переберите ассоциативный массив и выведите ключ и значение.
+?>
