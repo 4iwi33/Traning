e31d539ade57da88a748096e3a02bb58c58498ea
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-10 22:13:10 +0300

    sm

--- All/hw8.php
+++ All/hw8.php
@@ -1,10 +1,20 @@
-<?
-header ("Content-type: image/png");
-$im = imagecreatetruecolor(320, 240);
-$ink = imagecolorallocate($im, 255, 255, 255);
+<!DOCTYPE html>
+<html lang="en">
 
-// Стандартный прямоугольник
-imagerectangle($im,80,60,240,180,$ink);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HW8</title>
+    <link rel="stylesheet" href="hw8.css">
+</head>
 
-imagepng($im);
-imagedestroy($im);
+<body>
+    <div class="style" style="width: <?= $_GET['a'] ?>px; height: <?= $_GET['b'] ?>px; background-color: <?= $_GET['c'] ?>">
+        <?php
+        $S = $_GET['b'] * $_GET['a'];
+        echo "<center> square  <br>" . $s . "px<sup>2";
+        ?>
+    </div>
+</body>
+
+</html>
