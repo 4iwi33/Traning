0a900aa5058f612d9776af75428cd32965771022
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-23 22:49:12 +0300

    uppdate

--- logical operation/array/arr+for.php
+++ logical operation/array/arr+for.php
@@ -1,9 +1,28 @@
-<?php
-$list = array("Hello",18,true); //$list название массива||в скобках указываем элементы массива
+<!DOCTYPE html>
+<html lang="en">
 
-print_r($list);//в скобках указываем имя массива который хотим поверить$
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-echo count($list);//с помощюю функции count можно родчитать количество элементов
-echo "<br>";
+<body>
+    <?php
+    $list = array("Hello", 18, true);
+    echo $list[0]; //выбираем элемент котрый хотим вывести
 
-echo $list[0]; //выбираем элемент котрый хотим вывести
+    echo "<br>";
+
+    print_r($list); //в скобках указываем имя массива который хотим поверить$
+
+    echo "<br>";
+
+    for ($i = 0; $i < count($list); $i++) {
+        echo "Элемент с индеком $i равен - " . $list[$i];
+        echo "<br>";
+    }
+    ?>
+</body>
+
+</html>
