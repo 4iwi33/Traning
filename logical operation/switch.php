a3df3a559f3a451682c16d4f71b10114413210fe
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-23 19:09:37 +0300

    add new change

--- logical operation/switch.php
+++ logical operation/switch.php
@@ -10,6 +10,26 @@
 
 <body>
     <?php
+    // switch-переключатель
+    //зададим значение $a
+    $a = 21; //сдесь указываем case который хотим задействовать
+
+    switch ($a) {
+        case 16:
+            echo "Тебе 16 лет";
+            break;
+        case 18:
+            echo "Вам 18 лет";
+            break;
+        default: //после default ставить break не нужно
+            echo "Мне $a лет";
+    }
+
+
+    echo "<hr>";
+
+    //exc#2
+
     $a = "1";
     switch ($a) {
         case 1:
@@ -20,21 +40,24 @@
             break;
     }
 
-//     $favcolor = "blue";
+    echo "<hr>";
 
-// switch ($favcolor) {
-//     case "red":
-//         echo "Your favorite color is red!";
-//         break;
-//     case "blue":
-//         echo "Your favorite color is blue!";
-//         break;
-//     case "green":
-//         echo "Your favorite color is green!";
-//         break;
-//     default:
-//         echo "Your favorite color is neither red, blue, nor green!";
-// }
+    //хороший пример с цветом как аботает переключатель
+    $favcolor = "red";
+
+    switch ($favcolor) {
+        case "red":
+            echo "Your favorite color is red!";
+            break;
+        case "blue":
+            echo "Your favorite color is blue!";
+            break;
+        case "green":
+            echo "Your favorite color is green!";
+            break;
+        default:
+            echo "Your favorite color is neither red, blue, nor green!";
+    }
     ?>
 </body>
 
