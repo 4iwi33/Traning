fd5bb763adc6d5b4d9f6228ec6b079fdc747ee89
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-23 18:16:44 +0300

    delete

--- All/chek.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ($_POST["name"] == "") {
-    echo "Введите имя. <a href='/'>Исправить</a>";
-}
-header("Location: forms.php");
-?>
--- All/page.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$title = "Главная страница";
-include "header.php";
-echo "Тело документа";
-include "footer.php";
-?>
