468e50c65dc49001192015638d1f14b6025961b7
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-02-24 22:09:15 +0300

    upp

--- logical operation/array/associative_arr.php
+++ logical operation/array/associative_arr.php
@@ -4,12 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>array</title>
 </head>
 
 <body>
 
     <?php
+    //hello = привет
+    //ключ = значение
+    $arr = array("hello" => "привет", "world" => "мир");
+    $arr["me"] = "мне";
+    $arr["age"] = 20; //добавим новое зачение с ключём "age"
+    $arr["age"] = 23;//так же можно заменять элементы указав ключ
+
+    //теперь выводим значение через ключ
+    echo $arr["hello"] . " " . $arr["world"] . " " .  $arr["me"] .  " " . $arr["age"]; // " " так можно ставить пробел
+
+
+    //  print_r($arr);
+
+
+
 
     ?>
 </body>
