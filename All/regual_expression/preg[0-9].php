98e3e8cf8ecbccc5d3447fc7b5a74eb4b027507d
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-02 21:40:26 +0300

    ceaate [  ]

---
+++ All/regual_expression/preg[0-9].php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>[0-9]</title>
+</head>
+
+<body>
+    <?php
+    $str = "54657448---9807969";
+    //символ"\d+" находит первое число
+    //символ"\-" означает символьный интервал
+    //символ"\-{2,}" находит два и более символов
+
+
+    preg_match('/\d+\-{2,}\d+/', $str, $match);
+    // 
+    var_dump($match);
+    echo "<hr>";
+    print_r($match);
+    ?>
+</body>
+
+</html>
