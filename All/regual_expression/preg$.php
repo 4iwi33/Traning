52f258f2603aacd7ae3d7f1e773bcf33c05f5ead
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-02 19:05:06 +0300

    ddd

---
+++ All/regual_expression/preg$.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>preg$</title>
+</head>
+
+<body>
+    <?php
+    $str = 'test string end';
+    preg_match('/end$/', $str, $match);//проверим заканчивается ли строчка со слова с символом"$" 
+    var_dump($match);
+    ?>
+</body>
+
+</html>
