23a56b074b70ce37ed1506c38339bd682eded39b
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-02 18:53:57 +0300

    create notes

---
+++ All/regual_expression/preg_match.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>preg</title>
+</head>
+
+<body>
+
+    <?php
+    $str = 'test string test test test';
+    // preg_match('/test/',$str);
+    //preg_match — Выполняет проверку на соответствие регулярному выражению
+    if (preg_match('/test/', $str)) { //проверяем существование и выводим
+        echo "Success";
+    } else {
+        echo "False";
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ All/regual_expression/preg_mutch2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>preg2</title>
+</head>
+
+<body>
+
+    <?php
+    $str = 'test string test test test';
+    // preg_match('/test/',$str);
+    //preg_match — Выполняет проверку на соответствие регулярному выражению
+    preg_match('/^string/', $str, $match);//проверим наинается ли строчка сол слова с"^" 
+    var_dump($match);
+
+    ?>
+
+</body>
+
+</html>
