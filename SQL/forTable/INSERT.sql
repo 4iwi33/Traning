31cef5bffeca91875be39320b0188ffb6490ad8b
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-21 21:46:55 +0300

    add
    INSERT

---
+++ SQL/forTable/INSERT.sql
@@ -0,0 +1,6 @@
+--Оператор "INSERT" вставляет новые строки в существующую таблицу
+INSERT INTO test 
+(name, bio, birth, email) 
+--VALUES (значение)
+VALUES('Alex', 'Programmer', '2050-04-04', 'test@mail.ru')
+--Сдесь задали значение каждому столбцу
