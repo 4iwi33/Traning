194bc3d548a69e190d1f20af575e3980ec4dc5dd
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-22 16:39:53 +0300

    updt

--- SQL/forTable/UPDATE.sql
+++ SQL/forTable/UPDATE.sql
@@ -1,6 +1,11 @@
 -- Оператор "UPDATE" нужен для обновления данных
--- Оператор "WHERE" где изменить в нашем случае в id
+-- Оператор "WHERE" где изменить, в нашем случае в id
 -- Оператор "SET" выбираем имя столба
 UPDATE `test` 
 SET `name` = 'Max' 
 WHERE id = 5;
+
+--поменяли name и email , тем столбцам у которых id больше 4
+UPDATE test 
+SET `name` = 'Bobik', `email` = 'ivan@mail.ru'
+WHERE id > 4;
