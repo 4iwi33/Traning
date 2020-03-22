644a166cfcf9fa1873991c58ccd3cad497a06615
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-03-22 19:54:24 +0300

    SELECT
    SELECT:
    id
    name

--- SQL/forTable/SELECT.sql
+++ SQL/forTable/SELECT.sql
@@ -1,3 +1,21 @@
 -- Оператор "SELECT" выбрать 
 -- Оператор "SELECT *" выбрать всё 
-SELECT *   
+
+--Выведем в нашей базе таблицу lol
+SELECT * 
+FROM lol;
+
+--Выведем только name и bio 
+SELECT name, bio
+FROM test ;
+
+-----------------------------------------
+SELECT id, name --выведем id и name 
+FROM test       --в таблице test    
+WHERE  id > 2   --где id больше двух 
+AND id <4;      --и id меньше четырёх    
+-----------------------------------------
+SELECT id, name --выведем id и name 
+FROM test       --в таблице test    
+WHERE  id >= 2   --где id больше двух включительно
+AND id <4;      --и id меньше четырёх    
