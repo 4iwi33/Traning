2b881dda6a8218f65fdf7831cedb57a78ab0986b
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-05 21:09:29 +0300

    add

---
+++ new fol/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbase = 'test';
+
+    $db = mysqli_connect($dbhost, $dbuser, $dbpass) or die("error to connect");
+    if (!$db) 
+    {
+        die("error to connect");
+    }
+    mysqli_select_db($db, $dbase) or die("error to connect");
+    $select = mysqli_query($db, "SELECT id,short FROM testtable");
+    while ($row = mysqli_fetch_array($select)) 
+    {
+        echo $row;
+    }
+
+    ?>
+</body>
+
+</html>
