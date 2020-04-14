bdf82cd9ce7287bc4709143dcd81298ed1010d57
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-14 17:52:14 +0300

    ceate

---
+++ filter_walk_map/array_walk.php
@@ -0,0 +1,25 @@
+<?php
+// "array_walk" - Применить пользовательскую функцию к каждому члену массива
+
+// Возвращает TRUE в случае успешного завершения или FALSE в случае возникновения ошибки.
+
+$fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+
+function test_alter(&$item1, $key, $prefix) 
+{
+    $item1 = "$prefix: $item1";
+}
+
+function test_print($item2, $key) 
+{
+    echo "$key. $item2<br />\n";
+}
+
+echo "Before ...:\n";
+array_walk($fruits, 'test_print');
+
+array_walk($fruits, 'test_alter', 'fruit');
+echo "... and after:\n";
+
+array_walk($fruits, 'test_print');
+?>
