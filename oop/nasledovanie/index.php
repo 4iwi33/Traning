69a02d09eba20f4aa40f631c90be7d4fe1851bd5
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-20 20:32:35 +0300

    upd

--- oop/nasledovanie/index.php
+++ oop/nasledovanie/index.php
@@ -1,2 +1,30 @@
 <?php
 
+class User 
+{
+    function __construct($name, $pass, $email, $city)
+    {
+        $this->name = $name;
+        $this->pass = $pass;
+        $this->email = $email;
+        $this->city = $city;
+    }
+
+    function getInfo()
+    {
+        return "{$this->name}". "{$this->pass}". "{$this->email}". "{$this->city}";
+    }
+}
+
+$obj = new User("Ivan", "54321", "alex@mail.com", "Moscow");
+echo $obj->getInfo();
+
+// "extends" - наследование
+class Moderator extends User // класс Moderator наследует класс User
+{
+    public $info;
+    public $rights;
+}
+
+$moder = new Moderator("Alex", "12345", "alex@mail.com", "Minsk");
+echo $moder->getInfo();
