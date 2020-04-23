0ff83de675a5a714ff2a5c687b73ddae4f32ac01
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-23 21:42:41 +0300

    upd

--- oop/static/index.php
+++ oop/static/index.php
@@ -2,7 +2,16 @@
 
 class Lol
 {
-    public static $name;
+    public static $name; // статическое свойство
+
+    public static function hello()  // статическй метод
+    {
+        echo "Hello";
+        return self::$name;
+    }
 }
 Lol::$name = "Alex"; // задали свйство значение
-echo Lol::$name;
+echo Lol::$name;    //вывели
+echo Lol::hello(); //обращение
+
+//если в методе нет пееменой $this , то этот метод явл статическим
