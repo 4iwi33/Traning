fd2dd644e6319266cf3aa3bad80a85dbe0275ef9
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-27 14:23:14 +0300

    updt

--- oop/trait/index.php
+++ oop/trait/index.php
@@ -1,2 +1,28 @@
 <?php
 
+// Трейт - это механизм обеспечения повторного использования кода в языках
+//  с поддержкой только одиночного наследования, таких как PHP. 
+trait Hello
+{
+    public function sayHello()
+    {
+        echo "Hello ";
+    }
+}
+
+trait World
+{
+    public function sayWorld()
+    {
+        echo "World";
+    }
+}
+
+class myHelloWorld
+{
+    use Hello, World;
+}
+
+$obj = new myHelloWorld();
+$obj->sayHello();
+$obj->sayWorld();
