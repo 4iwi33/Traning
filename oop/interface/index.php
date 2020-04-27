64892720b85e46a69d0a6eed4bde38f8b0cd7449
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-27 13:42:58 +0300

    adding to index
    add interface

--- oop/interface/index.php
+++ oop/interface/index.php
@@ -1,2 +1,28 @@
 <?php
-interface 
+
+// нтерфейс это шаблоны ,  
+interface FirstInterface
+{
+    public function getName();
+}
+
+interface SecondInterface
+{
+    public function getStatus();
+}
+
+class Test implements FirstInterface, SecondInterface  // класс Test имплементирует класс FirstInterface 
+{
+    public $name = "Alex";
+    public $status = "Admin";
+
+    public function getName()
+    {
+        echo $this->name;
+    }
+
+    public function getStatus()
+    {
+        echo $this->status;
+    }
+}
