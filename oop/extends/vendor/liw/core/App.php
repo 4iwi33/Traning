3d4c253d95db8224be136ede77a1dab3b54a6513
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-05-23 18:03:13 +0300

    add something

---
+++ oop/extends/vendor/liw/core/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace liw\core;
+
+class App
+{
+    public function __construct()
+    {
+        echo "созд экземпляр класса из папки vendor-liw-core";
+    }
+}
---
+++ oop/extends/web/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . "/../vendor/liw/core/App.php";
+
+require __DIR__ . "/../app/App.php";
