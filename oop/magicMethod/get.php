db769006050ff47bfd83ac06e0fa42441be03127
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-06-05 14:03:54 +0300

    Update get.php

--- oop/magicMethod/get.php
+++ oop/magicMethod/get.php
@@ -2,19 +2,26 @@
 
 class GetSet
 {
-    private $number = 1;
+    private $number = 1; // свойство класса
 
-    public function __get($name)
+    public function __get($name) //функция, магический метод __get, один аргумент name 
     {
         echo "Y Get {$name}";
     }
 
-    public function __set($name, $value)
+    public function __set($name, $value) // функция, __set, в ней два аргумета name и value
     {
         echo "Y Set {$name} to {$value}";
     }
 }
 
-$obj = new GetSet();
-echo $obj->$number;
-echo $obj->$number = 689;
+$obj = new GetSet(); // создали экземпляр класса
+echo $obj->$number; // выводим экзкмпляр класса
+echo $obj->$number = 689; // меняем значение
+/*
+Метод __set() будет выполнен при записи данных в недоступные
+(защищенные или приватные) или несуществующие свойства.
+
+Метод __get() будет выполнен при чтении данных из недоступных 
+(защищенных или приватных) или несуществующих свойств.
+*/
