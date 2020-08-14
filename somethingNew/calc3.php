cb77f827c9027e3dbedc0cc195fc867ae6e9ddac
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-08-14 20:10:27 +0300

    add

---
+++ somethingNew/calc3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" id="num1" value="0">
+        <span>+</span>
+        <input type="text" id="num2" value="0">
+        <span>=</span>
+        <span id="demo">0</span>
+    </form>
+
+    <script src="calc3.js"></script>
+</body>
+
+</html>
